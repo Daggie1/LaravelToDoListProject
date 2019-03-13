489bb82d5e72c65a11f6621dd8a0b26e9196e39e
Anonymous <daggyooks@gmail.com>
2019-03-13 10:09:51 +0300

    adding delete and update task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function get_add_task_form(){
+        return view('task.add_task_form');
+}
+    public function edit($id){
+        $task= Task::find($id);
+        return view('tasks.edit_form',['task'=>$task]);
+    }
+    public function update(Request $request,$id){
+        $this->validate($request,[
+            'title'=>'required',
+            'deadline'=>'required',
+            'description'=>'required' ]);
+        $task = Post::find($id);
+        $task->title = $request->title;
+        $task->description = $request->description;
+        $task->deadline = $request->deadline;
+        $task->save();
+        return redirect()->route('home')->with('success', 'task has been updated successfully!');
+    }
+    public function deleteTask($id) {
+        $task = Task::find($id);
+        $task->delete();
+        return redirect()->route('home')->with('success', 'task has been deleted successfully!');
+    }
 }
